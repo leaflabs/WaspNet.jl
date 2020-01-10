e3a411c39e64467fc8da1e7571e34cf5ee65cbd4
Sam Buercklin <buercklin@leaflabs.com>
2020-01-10 11:12:23 -0500

    added more neuron tests, commented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,41 @@ using nnsim, Test
 @testset "nnsim Tests" begin
 
     @testset "Neurons" begin
-        lif = nnsim.LIF()
-        @test lif.state[1] == -55.
+        @testset "LIF" begin
+            lif = nnsim.LIF()
+            @test lif.state[1] == lif.v0        # Constructor works
 
-        update!(lif, 1, 0, 0)
-        @test lif.state[1] == -54.
+            update!(lif, 1, 0, 0)
+            @test lif.state[1] == lif.v0 + 1.   # Adding input to state without time evolution
 
-        reset!(lif)
-        @test lif.state[1] == -55.
+            update!(lif, 0, 0.001, 0)
+            @test lif.state[1] != lif.v0 + 1    # Time evolution changes the state
+
+            reset!(lif)
+            @test lif.state[1] == lif.v0        # Reset works
+        end
+
+        @testset "Izhikevich" begin
+            izh = nnsim.Izh()            
+            v0 = izh.v0
+            u0 = izh.u0
+            @test all(izh.state .== [v0, u0])   # Constructor works
+
+            update!(izh, 1, 0, 0)
+            @test begin                         # Adding input to state without time evolution
+                all(izh.state .== [v0 + 1., u0])    
+            end                                 
+
+            update!(izh, 0, 0.001, 0)           
+            @test begin                         # Time evolution changes the state
+                all(izh.state .!= [v0 + 1., u0])
+            end                                 
+
+            reset!(izh)
+            @test begin
+                all(izh.state .== [v0, u0])      
+            end                                 # Reset works
+        end
     end
 
     @testset "Layers" begin
@@ -20,21 +47,22 @@ using nnsim, Test
         lif2 = nnsim.LIF()
         W = zeros(2,2)
         W[1,1] = 1.
+        W[1,2] = 2.
         W[2,2] = 1.
 
         L = Layer([lif1, lif2], zeros(2), W, 2)
 
         update!(L, [0., 0.], 0.001, 0.)
-        @test lif1.state[1] == lif2.state[1]
+        @test lif1.state[1] == lif2.state[1]    # Both neurons are initialized to same state
         s10 = lif1.state[1]
         s20 = lif2.state[1]
 
         update!(L, [2., 3.], 0., 0.)
-        @test lif1.state[1] == s10 + 2.
-        @test lif2.state[1] == s20 + 3.
+        @test lif1.state[1] == s10 + 8.         # Inputs are routed correctly
+        @test lif2.state[1] == s20 + 3.         
 
         reset!(L)
-        @test lif1.state[1] == -55.
+        @test lif1.state[1] == -55.             # All neurons reset
         @test lif2.state[1] == -55.
     end
 
@@ -42,6 +70,5 @@ using nnsim, Test
 
         
     end
-
 
 end;
