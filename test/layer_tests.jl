630f22a3ca879ef182003bfdbd7d176d2300319d
Sam Buercklin <buercklin@leaflabs.com>
2020-07-01 18:25:45 -0400

    updated tests for new syntax and added tests for array-of-arrays

--- test/layer_tests.jl
+++ test/layer_tests.jl
@@ -19,6 +19,7 @@
     # Inputs are routed correctly
     @test begin                             
         update!(L, [[2., 3., 4.]], 0., 0.)
+        println(WaspNet.get_neuron_states(L))
         all(WaspNet.get_neuron_states(L) .== [8., 11.])
     end
 
@@ -76,3 +77,46 @@ end
     end
 end
 
+@testset "Multiple Connections Layer" begin
+    N0 = 2
+    N1 = 3
+    N2 = 4
+    neurons = [WaspNet.identity() for _ in 1:N1]
+    conns = [0,2]
+    W = Array{Array{Float64,2},1}()
+    push!(W, randn(N1, N0))
+    push!(W, randn(N1, N2))
+
+    layer = Layer(neurons, W, conns)
+
+    # Testing that update works at all
+    @test begin
+        update!(layer, [zeros(N0), zeros(N1), zeros(N2)], 0., 0.)
+        all([all(n.state .== [0]) for n in layer.neurons])
+    end
+
+    # Testing that input from the first + third layers affect the state
+    # Have to test approximate equality with ≈, presumbly from FP errors
+    @test begin
+        update!(layer, [ones(N0), zeros(N1), zeros(N2)], 0., 0.)
+        all(WaspNet.get_neuron_states(layer) .≈ sum.(eachrow(W[1])))
+    end
+    reset!(layer)
+    @test begin
+        update!(layer, [zeros(N0), zeros(N1), ones(N2)], 0., 0.)
+        all(WaspNet.get_neuron_states(layer) .≈ sum.(eachrow(W[2])))
+    end
+    @test begin
+        update!(layer, [ones(N0), zeros(N1), ones(N2)], 0., 0.) 
+        all(
+            WaspNet.get_neuron_states(layer) .≈ 
+                sum.(eachrow(W[1])) .+ sum.(eachrow(W[2]))
+            )
+    end
+
+    # Testing that we don't let input from the middle layer affect the state
+    @test begin
+        update!(layer, [zeros(N0), ones(N1), zeros(N2)], 0., 0.)
+        all(WaspNet.get_neuron_states(layer) .== [0; 0; 0])
+    end
+end
