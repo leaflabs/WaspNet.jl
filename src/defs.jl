ce231638c9b0c467c027f1eafff5d0abc68794bf
Sam Buercklin <buercklin@leaflabs.com>
2020-07-29 13:08:35 -0400

    tabs-to-spaces in defs.jl

--- src/defs.jl
+++ src/defs.jl
@@ -1,36 +1,36 @@
 """
-	update!(ne::WaspnetElement, input, dt, t)
+    update!(ne::WaspnetElement, input, dt, t)
 
 Evolve the `WaspnetElement` subject to an input `input` a duration of time `dt` starting from time `t`.
 """
 function update!(ne::WaspnetElement, input, dt, t) end
 
 function update!(ne::AbstractNeuron, input, dt, t)
-	warning("Neurons should use update, not update! function")
-	return update(ne, input, dt, t)
+    warning("Neurons should use update, not update! function")
+    return update(ne, input, dt, t)
 end
 
 """
-	reset!(ne::WaspnetElement)
+    reset!(ne::WaspnetElement)
 
 Reset the `WaspnetElement` to a default state defined by the method.
 """
 function reset!(ne::WaspnetElement) end
 
 function reset!(ne::AbstractNeuron, input, dt, t)
-	warning("Neurons should use reset, not reset! function")
-	return reset(ne, input, dt, t)
+    warning("Neurons should use reset, not reset! function")
+    return reset(ne, input, dt, t)
 end
 
 """
-	reset(n::AbstractNeuron)
+    reset(n::AbstractNeuron)
 
 Resets the neuron and returns the new struct with original values but the state updated.
 """
 function reset(n::AbstractNeuron) end
 
 """
-	simulate!(element<:WaspnetElement, input, dt, tf, t0 = 0.; track_state=false, kwargs...)
+    simulate!(element<:WaspnetElement, input, dt, tf, t0 = 0.; track_state=false, kwargs...)
 
 Simulates a given `WaspnetElement` element from times `t0` to `tf` with increments of `dt` subject to an input `input`, storing the results in a SimulationResult instance.
 
@@ -47,21 +47,21 @@ Note that there are `floor((tf-t0)/dt)+1` samples returned; the first sample poi
 function simulate!(ne::WaspnetElement, input, dt, tf, t0 = 0.; track_state=false, kwargs...) end
 
 """
-	function get_neuron_count(ne::WaspnetElement)
+    function get_neuron_count(ne::WaspnetElement)
 
 Returns the number of neurons in the given `WaspnetElement`
 """
 function get_neuron_count(ne::WaspnetElement) end
 
 """
-	function get_neuron_outputs(ne::WaspnetElement)
+    function get_neuron_outputs(ne::WaspnetElement)
 
 Returns the current output of all neurons in the given `WaspnetElement`
 """
 function get_neuron_outputs(ne::WaspnetElement) end
 
 """
-	function get_neuron_outputs(ne::WaspnetElement)
+    function get_neuron_outputs(ne::WaspnetElement)
 
 Returns the current state of all neurons in the given `WaspnetElement`
 """
