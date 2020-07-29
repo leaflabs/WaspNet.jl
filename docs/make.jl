f61c5a87fd474bf3fe0d6cad1e01b5730e3bed99
Sam Buercklin <buercklin@leaflabs.com>
2020-07-29 16:26:21 -0400

    added index.html to docs sidebar

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, WaspNet
 makedocs(
 	sitename="WaspNet",
 	pages = [
+		 "Home" => "index.md",
 		# "Overview of WaspNet" => "overview.md",
 		"Example" => "example.md",
 		"Reference" => "reference.md"
