00c751e49ab585e068b8ba37f85a2463f8cdadeb
Sam Buercklin <buercklin@leaflabs.com>
2020-08-26 01:00:58 -0400

    LIF neuron updated to be compatible with DiffEq network

--- src/neurons/LIF.jl
+++ src/neurons/LIF.jl
@@ -1,57 +1,41 @@
 """
     LIF{T<:Number}<:AbstractNeuron 
 
-Contains the necessary parameters for describing a Leaky Integrate-and-Fire (LIF) neuron as well as the current membrane potential of the neuron.
+Contains the necessary parameters for describing a Leaky Integrate-and-Fire (LIF) neuron.
 
 # Fields
 - `τ::T`: Neuron time constant (ms)
-- `R::T`: Neuronal model resistor (MOhms)
-- `θ::T`: Threshold voltage (mV) - when state exceeds this, firing occurs.
-- `vSS::T`: Steady-state voltage (mV) - in the absence of input, this is the resting membrane potential.
-- `v0::T`: Reset voltage (mV) - immediately after firing, state is set to this.
-- `state::T`: Current membrane potential (mV)
-
-Different relative orders of threshold voltage, resting voltage, and reset voltage will produce different dynamics.
-The default values of resting > threshold >> reset allows for a baseline firing rate that can be modulated up or down.
+- `R::T`: Neuronal model resistor (kOhms)
+- `θ::T`: Threshold voltage (mV)
+- `I::T`: Background current injection (mV)
+- `v0::T`: Reset voltage (mV)
 """
 @with_kw struct LIF{T<:Number}<:AbstractNeuron 
-    τ::T = 8.         
-    R::T = 10.      
-    θ::T = -55.     
+    τ::T = 4.         
+    R::T = 6.      
+    θ::T = 30.      
+    I::T = 40.      
 
-    vSS::T = -50.
-    v0::T = -100. 
-    state::T = -100.
-    output::T = 0.     
+    v0::T = -55.     
 end
 
 """
-    update!(neuron::LIF, input_update, dt, t)
+    update(neuron::LIF, input_update, dt, t)
 
-Evolve an `LIF` neuron subject to a membrane potential step of size `input_update` a time duration `dt` starting from time `t`
-
-# Inputs
-- `input_update`: Membrane input charge (pC)
-- `dt`: timestep duration (s)
-- `t`: global time (s)
+Evolve and `LIF` neuron subject to a membrane potential step of size `input_update` a time duration `dt` starting from time `t`
 """
-function update(neuron::LIF, input_update, dt, t)
-    output = 0.
-    # If an impulse came in, add it
-    state = neuron.state + input_update * neuron.R / neuron.τ
-
-    # Euler method update
-    state += 1000 * (dt/neuron.τ) * (-state + neuron.vSS)
-
-    # Check for thresholding
-    if state >= neuron.θ
-        state = neuron.v0
-        output = 1. # Binary output
-    end
-
-    return (output, LIF(neuron.τ, neuron.R, neuron.θ, neuron.vSS, neuron.v0, state, output))
+function update(neuron::LIF, u, t)
+    dv = (-u[1] + neuron.R*neuron.I) / neuron.τ
+    return (dv, )
+end
+function event(neuron::LIF, u, t)
+    u[1] > neuron.θ
 end
 
 function reset(neuron::LIF)
-    return LIF(neuron.τ, neuron.R, neuron.θ, neuron.vSS, neuron.v0, neuron.v0, 0.)
+    return (neuron.v0, )
+end
+
+function state_size(neuron::LIF)
+    return 1
 end
