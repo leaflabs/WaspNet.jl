6c9d20d731849fc557c7dee0b68ed418ee298826
Sam Buercklin <buercklin@leaflabs.com>
2020-09-09 17:10:39 -0400

    input layer logic

---
+++ src/input_layer.jl
@@ -0,0 +1,23 @@
+struct InputMatrixLayer{M<:AbstractArray{<:Number, 2}, T<:AbstractArray{<:Number,1}}<:AbstractLayer
+    data::M
+    times::T
+    idx::Int
+end
+
+function InputMatrixLayer(data, dt, tf)
+    times = 0.:dt:tf
+    @assert (length(times) == size(data)[2]) "Time duration and length of input data must match"
+    return InputMatrixLayer(data, times, 1)
+end
+
+function update!(l::InputMatrixLayer, du, u, t)
+    return nothing
+end
+
+function event(l::InputMatrixLayer, u, t)
+    return false
+end
+
+function aff_element!(l::InputMatrixLayer, u, input, t)
+    return nothing
+end
