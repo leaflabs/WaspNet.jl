266eca82c57d1c8b412a74754380a501c97d45be
Sam Buercklin <buercklin@leaflabs.com>
2020-09-09 17:11:10 -0400

    started diffeq handling logic (broken)

--- src/utils.jl
+++ src/utils.jl
@@ -4,3 +4,6 @@ include("utilities/utils.jl")
 
 # A pruning framework to remove neurons from Networks and Layers
 include("utilities/pruning.jl") 
+
+# Code to handle setting up Diffeq problems and callbacks
+include("utilities/diffeq_handling")
